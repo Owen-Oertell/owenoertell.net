ab447257a0ee34f68fffb2a134914e265f288555
Owen Oertell <owen@oertell.net>
2021-03-29 12:28:01 -0400

    Adding minor style changes. Still a lot left.

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": (preferDefault(require("/home/oweno/Documents/personal-website/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---cache-dev-404-page-js": (preferDefault(require("/home/oweno/Documents/personal-website/.cache/dev-404-page.js"))),
   "component---src-templates-categories-list-template-js": (preferDefault(require("/home/oweno/Documents/personal-website/src/templates/categories-list-template.js"))),
   "component---src-templates-category-template-js": (preferDefault(require("/home/oweno/Documents/personal-website/src/templates/category-template.js"))),
   "component---src-templates-index-template-js": (preferDefault(require("/home/oweno/Documents/personal-website/src/templates/index-template.js"))),
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # personal-website
-My personal website (still in the works)
 [![Netlify Status](https://api.netlify.com/api/v1/badges/d28ac49d-ed9c-4d8d-b666-abcab620c6ca/deploy-status)](https://app.netlify.com/sites/awesome-yonath-2e36b3/deploys)
+My personal website (still in the works)
