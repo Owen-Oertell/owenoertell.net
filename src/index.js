82cba5636171bb84c502a1d4b92e13ddbcb6a436
Owen Oertell <owenoertell@Owens-MacBook-Pro-3.local>
2022-03-24 00:53:43 -0400

    change

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<App />} />
-    </Routes>
-  </BrowserRouter>,
+ // <BrowserRouter>
+  //  <Routes>
+ //     <Route path="*" element={<App />} />
+  //  </Routes>
+  //</BrowserRouter>,
 
   document.getElementById('root')
 );
