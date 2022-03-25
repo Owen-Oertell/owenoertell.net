413d2cc3c7204650472c8328ae35999075985f08
Owen Oertell <owenoertell@Owens-MacBook-Pro-3.local>
2022-03-24 20:45:35 -0400

    Adding final redirects 2

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const memoryHistory = createMemoryHistory(options);
-
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
